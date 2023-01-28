46524ff8f3438fb01b435a3a8b694b0a55634c69
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-29 01:32:48 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,33 @@
+export const item = {
+    "id": "79e7ded4-a11c-488c-ba72-fc24ca116401",
+    "artist_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/163.png",
+    "artist_details": "sport",
+    "artist_description": "Form sit student even position again sometimes. Degree partner agree southern entire detail. Charge partner against sea.",
+    "profile_list": [
+        {
+            "id": "575c4841-0738-4dcb-91e2-e057c1cef035",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/101.png",
+            "art_text": "statement"
+        },
+        {
+            "id": "3e070020-6137-4695-86ed-27be2dcce452",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/5.png",
+            "art_text": "same"
+        },
+        {
+            "id": "1409c65f-6c1e-4e4e-8027-822cc4bae051",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/199.png",
+            "art_text": "nation"
+        },
+        {
+            "id": "b495bc2f-c917-40eb-a92c-d483166f516c",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/285.png",
+            "art_text": "race"
+        },
+        {
+            "id": "477dd3da-348b-43c2-8af8-22fcedbacdbf",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/92.png",
+            "art_text": "analysis"
+        }
+    ]
+}
